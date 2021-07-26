63b3cd119277910e9f6e45c7f80b3234dbaae078
Anthony Cros <cros.anthony@gmail.com>
2021-07-26 10:27:49 -0400

    added renaming tests for group-bys

--- src/main/scala/galliatest/suites/multiple/GroupingTest.scala
+++ src/main/scala/galliatest/suites/multiple/GroupingTest.scala
@@ -4,22 +4,20 @@ import gallia._
 
 // ===========================================================================
 object GroupingTest extends gallia.testing.Suite {
-import TestDataO._
-import TestDataS._
+  import TestDataO._
+  import TestDataS._
 
-
-//private val Default03b = bobj('p -> Default01b, 'z -> false)
-  private val zzz1 = bobjs(Default03, bobj('p -> Default01b, 'z -> false), Default03)
-  private val zzz2 = bobjs(bobj('z -> true, 'p -> 8), bobj('z -> false, 'p -> 4))
+  private val Input1 = bobjs(Default03, bobj('p -> Default01b, 'z -> false), Default03)
+  private val Input2 = bobjs(bobj('z -> true, 'p -> 8), bobj('z -> false, 'p -> 4))
 
   // ---------------------------------------------------------------------------
-  private val IJEC = bobjs(bobj('f -> "foo", 'g -> Seq(1, 1)), bobj('f -> "foo2", 'g -> Seq(2) ))
-  private val IJED = bobjs(bobj('f -> "foo", _group -> Seq(bobj('g -> 1), bobj('g -> 1))), bobj('f -> "foo2", _group -> Seq(bobj('g -> 2)) ))
-  private val IJEE = bobjs(bobj('f -> "foo", 'GROUP -> Seq(bobj('g -> 1), bobj('g -> 1))), bobj('f -> "foo2", 'GROUP -> Seq(bobj('g -> 2)) ))
+  private val Output1 = bobjs(bobj('f -> "foo", 'g -> Seq(1, 1)), bobj('f -> "foo2", 'g -> Seq(2) ))
+  private val Output2 = bobjs(bobj('f -> "foo", _group -> Seq(bobj('g -> 1), bobj('g -> 1))), bobj('f -> "foo2", _group -> Seq(bobj('g -> 2)) ))
+  private val Output3 = bobjs(bobj('f -> "foo", 'GROUP -> Seq(bobj('g -> 1), bobj('g -> 1))), bobj('f -> "foo2", 'GROUP -> Seq(bobj('g -> 2)) ))
  
   // ===========================================================================
   override def test() {
-    zzz1.equivalents(res1)(
+    Input1.equivalents(res1)(
       _.group   ('p        ).by('z       ),
       _.groupOne(_.index(0)).by('z       ),
       _.groupOne(_.firstKey).by('z       ),
@@ -27,7 +25,7 @@ import TestDataS._
       _.grouping(_.field(_.firstKey).by(_.lastKey)) )
 
     // ---------------------------------------------------------------------------
-    zzz1.equivalents(res2)(
+    Input1.equivalents(res2)(
       _.groupBy ('z),
       _.groupBy ('z).asDefault,
       _.groupBy ('z).as(_group),
@@ -50,7 +48,7 @@ import TestDataS._
         bobj('f1 -> "foo1", 'g -> Seq(1, 1)),
         bobj('f1 -> "foo3", 'g -> Seq(3) )) )
 
-    zzz1
+    Input1
         .group('p).by('z)
         .transformObjects('p).using {
           _.squash(_.string('f), _.int('g)).using {
@@ -61,18 +59,31 @@ import TestDataS._
           bobj('z -> false, 'p -> 6)))
 
     // ===========================================================================
-    Default52.equivalents(IJEC)(
+    Default52.equivalents(Output1)(
        _.group('g).by('f) )
 
-    Default52.equivalents(IJED)(
+    Default52.equivalents(Output2)(
       _.groupBy('f),
       _.groupBy('f).asDefault,
       _.grouping(_.all.by('f)           ),
       _.grouping(_.all.by('f).asDefault ) )
 
-    Default52.equivalents(IJEE)(
+    Default52.equivalents(Output3)(
       _.groupBy('f)          .as('GROUP),
       _.grouping(_.all.by('f).as('GROUP)) )
+
+    // ===========================================================================
+    // renaming
+    Default52.group('g)      .by('f).check(bobjs(bobj('f -> "foo", 'g -> Seq(1, 1)), bobj('f -> "foo2", 'g -> Seq(2) )))
+    Default52.group('g ~> 'G).by('f).check(bobjs(bobj('f -> "foo", 'G -> Seq(1, 1)), bobj('f -> "foo2", 'G -> Seq(2) )))
+
+    yyy.group('f1 ~> 'F1, 'f2).by('g).check(bobjs(
+        bobj('g -> 1, _group -> Seq(bobj('F1 -> "foo1", 'f2 -> "foo2"), bobj('F1 -> "foo1", 'f2 -> "foo2"))), 
+        bobj('g -> 3, _group -> Seq(bobj('F1 -> "foo3", 'f2 -> "foo4")))))
+
+    yyy.group('f1, 'f2).by('g ~> 'G).check(bobjs(
+        bobj('G -> 1, _group -> Seq(bobj('f1 -> "foo1", 'f2 -> "foo2"), bobj('f1 -> "foo1", 'f2 -> "foo2"))), 
+        bobj('G -> 3, _group -> Seq(bobj('f1 -> "foo3", 'f2 -> "foo4")))))
 
     // ===========================================================================
     yyy.equivalents(
