d85553c9f6b5a6163b208faa65a47f90d795aace
Anthony Cros <cros.anthony@gmail.com>
2022-01-05 09:21:41 -0500

    Added some io tests

--- src/main/scala/galliatest/suites/IoTest.scala
+++ src/main/scala/galliatest/suites/IoTest.scala
@@ -11,7 +11,21 @@ object IoTest extends gallia.testing.Suite {
     // ---------------------------------------------------------------------------
     Seq(("foo", 1), ("bar", 2))
       .toHead { case (f, g) => bobj("f" -> f, "g" -> g) }
-        .check(bobjs(bobj('f -> "foo", 'g -> 1), bobj('f -> "bar", 'g -> 2)))    
+        .check(bobjs(bobj('f -> "foo", 'g -> 1), bobj('f -> "bar", 'g -> 2)))
+
+    // ---------------------------------------------------------------------------
+    // foreach (TODO: actually check output)
+    TestDataO.Default01.identity.foreach { _.printPrettyJson() }        
+    TestDataS.Default51.identity.foreach { _.printPrettyJson() }
+    
+    // ---------------------------------------------------------------------------
+    // print row/pretty-row (TODO: actually check output)
+    TestDataO.Default01.identity.printRow()
+    TestDataO.Default01.identity.printPrettyRow()
+    
+    TestDataS.Default51.identity.printPrettyJsons()
+    
+    TestDataO.Default01.identity.printRow()
   }
 
 }
